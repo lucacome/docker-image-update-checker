f50d56412b948cfdbb842c5419372681e0db3df1
Luca Comellini <luca.com@gmail.com>
2023-01-30 11:06:04 -0800

    Fix retrieving manifest for single platform (#23)

--- docker.sh
+++ docker.sh
@@ -17,14 +17,21 @@ get_manifests() {
     local digest=$2
     local token=$3
 
-    manifest_list=$(curl -sSL -H "Authorization: Bearer $token" \
-        -H "Accept: application/vnd.docker.distribution.manifest.list.v2+json,application/vnd.oci.image.index.v1+json" \
+    manifest_list=$(curl -sSL --dump-header headers -H "Authorization: Bearer $token" \
+        -H "Accept: application/vnd.docker.distribution.manifest.list.v2+json,application/vnd.oci.image.index.v1+json,application/vnd.docker.distribution.manifest.v2+json,application/vnd.oci.image.manifest.v1+json" \
         "https://index.docker.io/v2/${repo}/manifests/${digest}" 2>/dev/null)
 
     if jq -e -r '.errors[0].code' <<<"$manifest_list" >/dev/null; then
         error_code=$(jq -r '.errors[0].code' <<<"$manifest_list")
         message=$(jq -r '.errors[0].message' <<<"$manifest_list")
         error "Response from $repo\n code: $error_code\n message: $message"
+    fi
+
+    headers=$(cat headers | awk -F ': ' '{sub(/\r/,"\n",$2); print $1","$2}' | grep 'docker-content-digest\|content-type' | jq -R 'split(",") | {(if .[0] == "content-type" then "type" else "digest" end): .[1]}' | jq -s 'reduce .[] as $item ({}; . * $item)')
+    manifest_v2=$(jq -r '. | select(.type == "application/vnd.docker.distribution.manifest.v2+json" or .type == "application/vnd.oci.image.manifest.v1+json") | [{digest: .digest, platform: "linux/amd64"}]' <<<"$headers")
+    if [ ! -z "$manifest_v2" ]; then
+        echo "$manifest_v2"
+        return
     fi
 
     jq -r '[.manifests[] | select(.platform.architecture | contains ("unknown") | not) | {digest: .digest, platform: (.platform.os +"/"+ .platform.architecture)}]' <<<"$manifest_list"
@@ -37,7 +44,7 @@ get_layers() {
     local token=$3
 
     digestOutput=$(curl -sSL -H "Authorization: Bearer $token" \
-        -H "Accept: application/vnd.docker.distribution.manifest.v1+json,application/vnd.oci.image.manifest.v1+json" \
+        -H "Accept: application/vnd.docker.distribution.manifest.v2+json,application/vnd.oci.image.manifest.v1+json" \
         "https://index.docker.io/v2/${repo}/manifests/${digest}" 2>/dev/null)
 
     if jq -e -r '.errors[0].code' <<<"$digestOutput" >/dev/null; then
@@ -102,7 +109,7 @@ manifests_image=$(get_manifests $image_repo ${image_tag:-latest} $image_token)
 
 diff=false
 # loop through plafforms split by comma
-for platform in $(echo $platforms | tr ',' ' '); do
+for platform in $(echo $platforms | tr -s ',' ' '); do
     # get the digest for the platform
     digest_base=$(jq -r ".[] | select(.platform == \"$platform\") | .digest" <<<"$manifests_base")
 
