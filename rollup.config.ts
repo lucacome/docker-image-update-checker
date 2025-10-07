2b935c17dfe161df32ba61a6d9c6e85c636a81a3
Luca Comellini <luca.com@gmail.com>
2025-10-06 18:12:53 -0700

    format

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,7 +3,7 @@
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
-import json from '@rollup/plugin-json';
+import json from '@rollup/plugin-json'
 
 const config = {
   input: 'src/index.ts',
