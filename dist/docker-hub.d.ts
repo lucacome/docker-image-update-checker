eac2a7d8c66afa073b6345e2cee0db2f7f0dfa71
Luca Comellini <luca.com@gmail.com>
2026-03-04 16:20:44 -0800

    build

--- dist/docker-hub.d.ts
+++
@@ -1,8 +0,0 @@
-import { ContainerRegistry } from './registry.js';
-import { DockerAuth } from './auth.js';
-export declare class DockerHub extends ContainerRegistry {
-    constructor();
-    getToken(repository: string): Promise<string>;
-    getCredentials(): DockerAuth | undefined;
-}
-//# sourceMappingURL=docker-hub.d.ts.map
--- dist/gcr.d.ts
+++
@@ -1,8 +0,0 @@
-import { ContainerRegistry } from './registry.js';
-import { DockerAuth } from './auth.js';
-export declare class GoogleContainerRegistry extends ContainerRegistry {
-    constructor();
-    getToken(repository: string): Promise<string>;
-    getCredentials(): DockerAuth | undefined;
-}
-//# sourceMappingURL=gcr.d.ts.map
