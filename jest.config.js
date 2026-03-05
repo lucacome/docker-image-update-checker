cbf33f080939b8dc19a30022c6772e9576947b62
Luca Comellini <luca.com@gmail.com>
2026-03-04 16:18:21 -0800

    Update config

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ export default {
     '^.+\\.ts$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.eslint.json',
+        tsconfig: 'tsconfig.json',
         useESM: true,
       },
     ],
